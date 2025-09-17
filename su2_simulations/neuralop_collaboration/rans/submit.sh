121208e8f92548e090aff237af6b7cfc1c2c75c7
Atharva Aalok <atharvaaalok@gmail.com>
2025-09-16 22:50:02 -0700

    Update CPU count per task to 4 for 4 angles of attack.

--- su2_simulations/neuralop_collaboration/rans/submit.sh
+++ su2_simulations/neuralop_collaboration/rans/submit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --time=3:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=6
+#SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=4G
 #SBATCH --job-name shapeopt_rans
 #SBATCH --partition jjalonso
